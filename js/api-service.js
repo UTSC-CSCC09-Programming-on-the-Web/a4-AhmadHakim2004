565d7dd6302252e7f7f4d8d2300b5e60496c0164
AhmadHakim2004 <hakimahmad2004@gmail.com>
2025-05-29 13:14:06 -0400

    Implement api-service starter code

--- js/api-service.js
+++ js/api-service.js
@@ -1,6 +1,20 @@
 let apiService = (function () {
   let module = {};
 
+  const saveToLocalStorage = function (database) {
+    localStorage.setItem("database", JSON.stringify(database));
+    localStorage.setItem("imageId", imageId.toString());
+    localStorage.setItem("commentId", commentId.toString());
+  };
+
+  const loadFromLocalStorage = function () {
+    return JSON.parse(localStorage.getItem("database")) || [];
+  };
+
+  let imageId = parseInt(localStorage.getItem("imageId")) || 0;
+  let commentId = parseInt(localStorage.getItem("commentId")) || 0;
+  let database = loadFromLocalStorage();
+
   /*  ******* Data types *******
     image objects must have at least the following attributes:
         - (String) imageId 
@@ -17,17 +31,66 @@ let apiService = (function () {
         - (Date) date
   */
 
+  let Image = function (title, author, url) {
+    this.imageId = imageId++;
+    this.title = title;
+    this.author = author;
+    this.url = url;
+    this.date = new Date();
+    this.comments = [];
+  };
+
+  let Comment = function (imageId, author, content) {
+    this.commentId = commentId++;
+    this.imageId = imageId;
+    this.author = author;
+    this.content = content;
+    this.date = new Date();
+  };
+
   // add an image to the gallery
-  module.addImage = function (title, author, url) {};
+  module.addImage = function (title, author, url) {
+    let image = new Image(title, author, url);
+    database.unshift(image);
+    saveToLocalStorage(database);
+  };
 
   // delete an image from the gallery given its imageId
-  module.deleteImage = function (imageId) {};
+  module.deleteImage = function (imageId) {
+    let index = database.findIndex(function (image) {
+      return image.imageId === imageId;
+    });
+    if (index === -1) return null;
+    database.splice(index, 1);
+    saveToLocalStorage(database);
+  };
 
   // add a comment to an image
-  module.addComment = function (imageId, author, content) {};
+  module.addComment = function (imageId, author, content) {
+    let image = database.find(function (image) {
+      return image.imageId === imageId;
+    });
+    if (image) {
+      let comment = Comment(imageId, author, content);
+      image.comments.unshift(comment);
+      saveToLocalStorage(database);
+    }
+  };
 
   // delete a comment to an image
-  module.deleteComment = function (commentId) {};
+  module.deleteComment = function (commentId) {
+    for (let image of database) {
+      let index = image.comments.findIndex(function (comment) {
+        return comment.commentId === commentId;
+      });
+
+      if (index !== -1) {
+        image.comments.splice(index, 1);
+        saveToLocalStorage(database);
+        return;
+      }
+    }
+  };
 
   return module;
 })();
