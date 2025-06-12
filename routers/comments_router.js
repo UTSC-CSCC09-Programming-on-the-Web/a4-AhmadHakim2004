2155a91879771c89768c5c0c7a58dadc370b427c
AhmadHakim2004 <hakimahmad2004@gmail.com>
2025-06-12 09:28:02 -0400

    Add DELETE comment route

--- routers/comments_router.js
+++ routers/comments_router.js
@@ -1,3 +1,19 @@
 import { Router } from "express";
+import { Comment } from "../models/comments.js";
 
 export const commentsRouter = Router();
+
+commentsRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const comment = await Comment.findByPk(req.params.id);
+    if (!comment) {
+      return res
+        .status(404)
+        .json({ error: `comment with id=${req.params.id} not found.` });
+    }
+    await comment.destroy();
+    return res.json(comment);
+  } catch (e) {
+    return res.status(400).json({ error: "Cannot delete comment" });
+  }
+});
