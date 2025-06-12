8b5f12f5b5b8fb06532cc70f2a97a701a713e928
AhmadHakim2004 <hakimahmad2004@gmail.com>
2025-06-11 23:52:38 -0400

    Add POST image route

--- routers/images_router.js
+++ routers/images_router.js
@@ -1,3 +1,33 @@
 import { Router } from "express";
+import multer from "multer";
+import { validateInput } from "../utils/validate-input.js";
+import { Image } from "../models/images.js";
 
+const upload = multer({ dest: "uploads/" });
 export const imagesRouter = Router();
+
+imagesRouter.post(
+    "/", 
+    upload.single("picture"),
+    async function (req, res, next) {
+        const schema = [
+            { name: "title", required: true, type: "string", location: "body"},
+            { name: "author", required: true, type: "string", location: "body"},
+            { name: "picture", required: true, type: "file", location: "file"}
+        ];
+
+        if (!validateInput(req, res, schema)) return;
+
+        try {
+            const image = await Image.create({
+                title: req.body.title,
+                author: req.body.author,
+                picture: req.file
+            });
+            return res.json(image);
+        }
+        catch (e) {
+            return res.status(400).json({ error: "Cannot create image" });
+        }
+    }
+);
