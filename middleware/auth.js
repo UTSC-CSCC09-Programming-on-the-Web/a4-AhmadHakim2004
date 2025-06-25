45762ad512d7aa2a74a1b417bae9b6b74976ce43
AhmadHakim2004 <hakimahmad2004@gmail.com>
2025-06-25 05:05:47 -0400

    Add authenticaion middleware

---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+import { extractTokenFromReq } from "../utils/token-helpers.js";
+
+export const isAuthenticated = async function (req, res, next) {
+    const token = await extractTokenFromReq(req);
+  if (!token) {
+    return res.status(401).json({ error: "Not Authenticated" });
+  }
+  next();
+};
